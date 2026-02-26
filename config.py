a8db2ed4bb0034d3393cdcea0d49617399f9b06a
Noureddine El Fatimi <noureddineelfatimi03@gmail.com>
2026-02-26 14:20:02 +0000

    set config

--- config.py
+++ config.py
@@ -3,12 +3,15 @@ import os
 
 load_dotenv()
 
-ollama_api_key=os.getenv("OLLAMA_API_KEY") or False
-ollama_base_url=os.getenv("OLLAMA_BASE_URL") or False
-ollama_model_name=os.getenv("OLLAMA_MODEL_NAME") or False
-openmeteo_base_url=os.getenv("OPENMETEO_BASE_URL") or False
+OLLAMA_API_KEY=os.getenv("OLLAMA_API_KEY") or False
+OLLAMA_BASE_URL=os.getenv("OLLAMA_BASE_URL") or False
+OLLAMA_MODEL_NAME=os.getenv("OLLAMA_MODEL_NAME") or False
+OPENMETEO_BASE_URL=os.getenv("OPENMETEO_BASE_URL") or False
+WEATHER_BASE_URL=os.getenv("WEATHER_BASE_URL") or False
+WEATHER_API_KEY=os.getenv("WEATHER_API_KEY") or False
+GEOCODING_URL=os.getenv("GEOCODING_URL") or False
 
-if not ollama_api_key or not ollama_base_url or not ollama_model_name or not openmeteo_base_url:
+if not OLLAMA_API_KEY or not OLLAMA_BASE_URL or not OLLAMA_MODEL_NAME or not OPENMETEO_BASE_URL or not WEATHER_API_KEY or not WEATHER_BASE_URL or not GEOCODING_URL:
     raise ValueError(
-        "Please set OLLAMA_API_KEY, OLLAMA_BASE_URL, OLLAMA_MODEL_NAME, OPENMETEO_BASE_URL via env var or code."
+        "Please set OLLAMA_API_KEY, OLLAMA_BASE_URL, OLLAMA_MODEL_NAME, OPENMETEO_BASE_URL, WEATHER_API_KEY, WEATHER_BASE_URL, GEOCODING_URL via env var or code."
     )
