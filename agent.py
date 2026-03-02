15107cb9c204314369cbc0d5708f00e8a3838205
Noureddine El Fatimi <noureddineelfatimi03@gmail.com>
2026-03-02 14:00:24 +0000

    add instructions to agent

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ from agents import Agent, Runner, set_default_openai_client, set_tracing_disable
 from config import OLLAMA_API_KEY, OLLAMA_BASE_URL, OLLAMA_MODEL_NAME
 from openai import AsyncOpenAI
 import asyncio
-from tools import get_current_weather, resolve_location, get_weather_forecast
+from tools import get_current_weather, resolve_location, get_weather_forecast, get_hourly_forecast,get_weather_alerts
 
 client = AsyncOpenAI(base_url=OLLAMA_BASE_URL, api_key=OLLAMA_API_KEY)
 
@@ -12,8 +12,16 @@ set_tracing_disabled(disabled=True)
 
 async def main():
     try:
-        agent = Agent( name="Assistant", instructions="You are a gentle weather information agent that provides informations about weather conditions", model=OLLAMA_MODEL_NAME, tools=[resolve_location, get_current_weather, get_weather_forecast])
-        result = await Runner.run(agent, "It's will raining tomorrow in Casablanca?")
+        agent = Agent( name="Assistant", instructions="""
+        You are a friendly and natural weather assistant.
+            - Answer directly and clearly.
+            - Be conversational and human-like.
+            - Avoid overly technical formatting.
+            - Summarize the important information first.
+            - Only include relevant details.
+            - If the user asks a yes/no question, answer it first before giving details.   
+        """, model=OLLAMA_MODEL_NAME, tools=[get_weather_alerts,get_current_weather, resolve_location, get_weather_forecast, get_hourly_forecast,])
+        result = await Runner.run(agent, "It's will raining today in Casablanca ?")
         print(result.final_output)
     except ModelBehaviorError:
         print("Model encountered an error")
